71ba7a3705f49b4e9c96786bba3ab8eea52c14b9
DaryaCherchenko <cherchenkodarya@gmail.com>
2025-06-08 11:51:47 +0300

    на ревью 1

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
-import fileMiddleware from '../middlewares/file'
+import {
+    checkImageContent,
+    checkMinFileSize,
+    upload,
+} from '../middlewares/file'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post('/', checkImageContent, upload.single('file'), checkMinFileSize, uploadFile)
 
 export default uploadRouter
